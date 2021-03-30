9d007e935f03101e8ffa17775a1dd832022add7d
shankarnagj <39335620+shankarnagj@users.noreply.github.com>
2021-03-30 08:16:24 +0530

    Add files via upload

---
+++ pythonProject/calc/basic_operations.py
@@ -0,0 +1,15 @@
+
+
+
+def add(a,b):
+    return a + b
+def sub(a,b):
+        return  a - b
+def mul(a,b):
+        return  b * a
+def div(a,b):
+        return a/b
+def floor_div(a,b):
+        return a//b
+def modulo_div(a,b):
+        return  a%b
---
+++ pythonProject/inputs.py
@@ -0,0 +1,4 @@
+from calc.basic_operations import *
+
+print(add(2,3))
+print((floor_div(3,2)))
